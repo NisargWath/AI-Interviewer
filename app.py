acedd4de578fcdae5bb7ed04f3e60e910a3b0b03
Nisarg Wath <nisargwath7@gmail.com>
2025-03-07 00:11:43 +0530

    added the paths

--- app.py
+++ app.py
@@ -44,6 +44,12 @@ QUESTION_BANK = [
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route("/phase1")
 def phase1():
